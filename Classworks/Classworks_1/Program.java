9482f56f8ad7887cee11f81985cce1ae1858802e
Sadekov Nadir <nadir_sa@mail.ru>
2019-04-23 19:55:02 +0300

    add Classwork_1 started

---
+++ Classworks/Classworks_1/Program.java
@@ -0,0 +1,33 @@
+class program{
+	public static void main (String[] args)
+
+	int number = 135
+
+	int a0, a1, a2, a3, a4, a5, a6, a7;
+	int i0, i1, i2, i3, i4, i5, i6, i7:
+
+	a0 = number % 2
+	i0 = number / 2
+	a1 = a0 % 2
+	i1 = a0 / 2
+	a2 = a1 % 2
+	i2 = i1 / 2
+	a3 = a2 % 2
+	i3 = i2 / 2
+	a4 = a3 % 2
+	i4 = i3 / 2
+	a5 = a4 % 2
+	i5 = i4 / 2
+	a6 = a5 % 2
+	i6 = i6 / 2
+	a7 = a6 % 2
+	i7 = i6 / 2
+
+	System.out.println (a7)
+	System.out.println (a6)
+	System.out.println (a5)
+	System.out.println (a4)
+	System.out.println (a3)
+	System.out.println (a2)
+	System.out.println (a1)
+}
