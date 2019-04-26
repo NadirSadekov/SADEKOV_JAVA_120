af6b574733e2a24c212462063cc28541e839d901
Sadekov Nadir <nadir_sa@mail.ru>
2019-04-26 22:16:51 +0300

    Homework_3 final version

--- Homeworks/Homeworks_3/Program.java
+++ Homeworks/Homeworks_3/Program.java
@@ -4,30 +4,32 @@ class Program {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
-        int current = scanner.nextInt(); 
+        int number0 = scanner.nextInt(); 
         
         int digitsSum = 0;
+
+        while (number0 != 0) {
+            digitsSum += number0 % 10;
+            number0 /= 10;
+        }
+        System.out.println("Сумма чисел: " + digitsSum);
+
+        int number1 = scanner.nextInt();
         int productOfNumbers = 1;
 
-        while (current > 0) {
-            digitsSum = digitsSum + current % 10;
-            current = current / 10;
-        }
-            System.out.println("Сумма чисел: " + digitsSum);
 
-            current = scanner.nextInt();
 
-        while (current > 0) {
-        if (digitsSum % 2 == 0 && current % 2 == 0) {
-            productOfNumbers = productOfNumbers * (current % 10);
-            digitsSum = digitsSum + current % 10;
-            current = current / 10;
-            System.out.println("Произведение чисел: " + productOfNumbers);
-            }    
-        else {
-            System.out.println("PLEASE REPEAT");
-            }      
-            current = scanner.nextInt();  
+        if( digitsSum % 2 == 0 ){
+            while (number1 % 2 != 0) {
+                System.out.println("Please repeat");
+                number1 = scanner.nextInt();
+            } 
         }
+
+        while(number1 != 0){
+                productOfNumbers *= number1 % 10;
+                number1 /= 10;
+            }
+        System.out.println("Произведение чисел: " + productOfNumbers);
     } 
 }
