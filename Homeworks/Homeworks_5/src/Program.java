2d0a650f01e1cd82e49c7bcbb759f0af97a38f44
Sadekov Nadir <nadir_sa@mail.ru>
2019-05-13 17:47:36 +0300

    Homework_5 and 6

--- Homeworks/Homeworks_5/src/Program.java
+++ Homeworks/Homeworks_5/src/Program.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Program {
+class Program {
+    public static void main(String[] args) {
+
+        System.out.print("Input array size: ");
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+        int a[] = new int[arraySize];
+        int localMax = 0;
+        int localMin = 0;
+
+        for (int i = 0; i < arraySize; i++)
+            a[i] = scanner.nextInt();
+
+        for (int i = 1; i < arraySize-1; i++)
+            if (a[i-1] > a[i] && a[i]< a[i+1])
+                localMin++;
+            else if (a[i-1] < a[i] && a[i] > a[i+1])
+                localMax++;
+        System.out.println("LOCAL MAX COUNT = " + localMax);
+        System.out.println("LOCAL MIN COUNT = " + localMin);
+    }
 }
--- Homeworks/Homeworks_6/src/Program.java
+++ Homeworks/Homeworks_6/src/Program.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Program {
+class Program {
+    public static void main(String[] args) {
+
+        System.out.print("Input array size: ");
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+        int a[] = new int[arraySize];
+        int max = a[0];
+        int maxIndex = 0;
+        int min = a[0];
+        int minIndex = 0;
+        int temp;
+
+        for (int i=0; i < arraySize; i++) {
+            a[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < arraySize; i++) {
+            if (a[i] > max) {
+                max = a[i];
+                maxIndex = i;
+            }
+        }
+        for (int i = 0; i < arraySize; i++) {
+            if (a[i] < min) {
+                min = a[i];
+                minIndex = i;
+            }
+        }
+        temp = a[maxIndex];
+        a[maxIndex] = a[minIndex];
+        a[minIndex] = temp;
+
+        for (int i=0; i < arraySize; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
